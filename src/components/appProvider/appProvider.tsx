1462758f03ad15b37e4ae8ba41325e218d600f45
Curia <admin@curiadesign.net>
2023-07-01 16:10:55 +1000

    chore: install saas-ui

--- src/components/appProvider/appProvider.tsx
+++ src/components/appProvider/appProvider.tsx
@@ -1,11 +1,21 @@
+import React from 'react';
 // Apollo
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from "@/lib/apolloClient";
+// saas ui
+import Link, { LinkProps } from 'next/link'
+import { SaasProvider } from '@saas-ui/react'
+
+const NextLink = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  (props, ref) => <Link ref={ref} {...props} />
+)
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ApolloProvider client={apolloClient}>
-      {children}
+      <SaasProvider linkComponent={NextLink}>
+        {children}
+      </SaasProvider>
     </ApolloProvider>
   );
 };
---
+++ src/lib/theme.ts
@@ -0,0 +1,5 @@
+
+import { extendTheme } from '@chakra-ui/react'
+import { theme as baseTheme } from '@saas-ui/react'
+
+export const theme = extendTheme({}, baseTheme)
